376e8b7911eb42f40cdcdad81e3bc64b74bf4bcb
Warley <wo7756720@gmail.com>
2025-09-09 19:40:03 -0300

    upgrade

--- lista6/exeercicio7.js
+++ lista6/exeercicio7.js
@@ -17,24 +17,24 @@ switch (opcao) {
         
         let volumeCone = (PI * Math.pow(raioCone, 2) * alturaCone) / 3;
         let areaCone = PI * raioCone * Math.sqrt(Math.pow(raioCone, 2) + Math.pow(alturaCone, 2));
-        console.log("Volume do cone reto:", volumeCone);
-        console.log("Área da superfície do cone reto:", areaCone);
+        console.log("Volume do cone reto:"+ volumeCone);
+        console.log("Área da superfície do cone reto:"+ areaCone);
         break;
 
     case 2:
         
         let volumeCilindro = PI * Math.pow(raioCilindro, 2) * alturaCilindro;
         let areaCilindro = 2 * PI * raioCilindro * (raioCilindro + alturaCilindro);
-        console.log("Volume do cilindro:", volumeCilindro);
-        console.log("Área da superfície do cilindro:", areaCilindro);
+        console.log("Volume do cilindro:"+ volumeCilindro);
+        console.log("Área da superfície do cilindro:"+ areaCilindro);
         break;
 
     case 3:
         
         let volumeEsfera = (4 / 3) * PI * Math.pow(raioEsfera, 3);
         let areaEsfera = 4 * PI * Math.pow(raioEsfera, 2);
-        console.log("Volume da esfera:", volumeEsfera);
-        console.log("Área da superfície da esfera:", areaEsfera);
+        console.log("Volume da esfera:"+ volumeEsfera);
+        console.log("Área da superfície da esfera:"+ areaEsfera);
         break;
 
     
